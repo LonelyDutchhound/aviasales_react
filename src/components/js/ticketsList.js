b6809f87d3b888e31233587c18c34419b709f347
Elena <e.feoktistova@catapod.xyz>
2019-08-30 18:16:40 +0300

    map fn instead of for-cicle

--- src/components/js/ticketsList.js
+++ src/components/js/ticketsList.js
@@ -5,89 +5,85 @@ import '.././css/ticket.css';
 function TicketsList ( props ) {
 console.log(props.tickets);
 
-  let ticketBlock = [];
-  for (let i = 0; i < props.tickets.length; i++) {
-    let { price, carrier, segments: [ { origin, destination, duration, date, stops: stops1 }, { origin:origin2, destination:destination2, duration:duration2, date:date2, stops: stops2 }] } = props.tickets[i];
+const ticketBlock = props.tickets.map(({ price, carrier, segments: [ { origin, destination, duration, date, stops: stops1 }, { origin:origin2, destination:destination2, duration:duration2, date:date2, stops: stops2 }] }, index) => {
+  const $stops1 = [];
+  for (var k = 0; k < stops1.length; k++) {
+    $stops1.push(stops1[`${k}`])
+  }
 
-    const $stops1 = [];
-    for (var k = 0; k < stops1.length; k++) {
-      $stops1.push(stops1[`${k}`])
-    }
+  const $stops2 = [];
+  for (var k = 0; k < stops2.length; k++) {
+    $stops2.push(stops2[`${k}`])
+  }
 
-    const $stops2 = [];
-    for (var k = 0; k < stops2.length; k++) {
-      $stops2.push(stops2[`${k}`])
-    }
+  const carrierLogo = `http://pics.avs.io/99/36/${ carrier }.png`
+  return (
+    <li className="ticket__list-element" key={ date }>
 
-    const carrierLogo = `http://pics.avs.io/99/36/${ carrier }.png`
+        <div className="ticket__block">
+          <div className="ticket__header">
+            <div className="ticket__price">{ price }</div>
+            <div className="ticket__complogo"><img src={ carrierLogo } /></div>
+          </div>
 
-    ticketBlock.push(
-      <li className="ticket__list-element" key={ i }>
-
-          <div className="ticket__block">
-            <div className="ticket__header">
-              <div className="ticket__price">{ price }</div>
-              <div className="ticket__complogo"><img src={ carrierLogo } /></div>
-            </div>
-
-            <div className="ticket__options">
-              <div className="ticket__route">
-                <div>
-                  { origin } - { destination }
-                </div>
-                <div>
-                  { date }
-                </div>
+          <div className="ticket__options">
+            <div className="ticket__route">
+              <div>
+                { origin } - { destination }
               </div>
-              <div className="ticket__duration">
-                <div>
-                  <span> в пути </span>
-                </div>
-                <div>
-                  { duration }
-                </div>
-              </div>
-              <div className="ticket__stops">
-                <div>
-                  <span> { $stops1.length} пересадки </span>
-                </div>
-                <div>
-                  { $stops1.join(', ') }
-                </div>
+              <div>
+                { date }
               </div>
             </div>
-
-            <div className="ticket__options">
-              <div className="ticket__route">
-                <div>
-                  { origin2 } - { destination2 }
-                </div>
-                <div>
-                  { date2 }
-                </div>
+            <div className="ticket__duration">
+              <div>
+                <span> в пути </span>
               </div>
-              <div className="ticket__duration">
-                <div>
-                  <span> в пути </span>
-                </div>
-                <div>
-                  { duration2 }
-                </div>
+              <div>
+                { duration }
               </div>
-              <div className="ticket__stops">
-                <div>
-                  <span> { $stops2.length} пересадки </span>
-                </div>
-                <div>
-                  { $stops2.join(', ') }
-                </div>
+            </div>
+            <div className="ticket__stops">
+              <div>
+                <span> { $stops1.length} пересадки </span>
+              </div>
+              <div>
+                { $stops1.join(', ') }
               </div>
             </div>
           </div>
-      </li>
-    );
-  }
 
+          <div className="ticket__options">
+            <div className="ticket__route">
+              <div>
+                { origin2 } - { destination2 }
+              </div>
+              <div>
+                { date2 }
+              </div>
+            </div>
+            <div className="ticket__duration">
+              <div>
+                <span> в пути </span>
+              </div>
+              <div>
+                { duration2 }
+              </div>
+            </div>
+            <div className="ticket__stops">
+              <div>
+                <span> { $stops2.length} пересадки </span>
+              </div>
+              <div>
+                { $stops2.join(', ') }
+              </div>
+            </div>
+          </div>
+        </div>
+    </li>
+  )
+});
+  
   return(
     <div className="ticket__container" >
       <ul className="ticket__list" >
