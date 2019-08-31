6ae669fd1ca586ba7f40fd6e9ee64f3f123b787a
Elena <e.feoktistova@catapod.xyz>
2019-08-31 14:11:31 +0300

    all for- -> map fn

--- src/components/js/ticketsList.js
+++ src/components/js/ticketsList.js
@@ -3,87 +3,57 @@ import '.././css/ticket.css';
 
 
 function TicketsList ( props ) {
-console.log(props.tickets);
 
-const ticketBlock = props.tickets.map(({ price, carrier, segments: [ { origin, destination, duration, date, stops: stops1 }, { origin:origin2, destination:destination2, duration:duration2, date:date2, stops: stops2 }] }, index) => {
-  const $stops1 = [];
-  for (var k = 0; k < stops1.length; k++) {
-    $stops1.push(stops1[`${k}`])
-  }
-
-  const $stops2 = [];
-  for (var k = 0; k < stops2.length; k++) {
-    $stops2.push(stops2[`${k}`])
-  }
-
+const ticketBlock = props.tickets.map(({ price, carrier, segments }, index) => {
   const carrierLogo = `http://pics.avs.io/99/36/${ carrier }.png`
+
   return (
-    <li className="ticket__list-element" key={ date }>
 
-        <div className="ticket__block">
-          <div className="ticket__header">
-            <div className="ticket__price">{ price }</div>
-            <div className="ticket__complogo"><img src={ carrierLogo } /></div>
-          </div>
+    <li className="ticket__list-element" key={ index }>
 
-          <div className="ticket__options">
-            <div className="ticket__route">
-              <div>
-                { origin } - { destination }
-              </div>
-              <div>
-                { date }
-              </div>
-            </div>
-            <div className="ticket__duration">
-              <div>
-                <span> в пути </span>
-              </div>
-              <div>
-                { duration }
-              </div>
-            </div>
-            <div className="ticket__stops">
-              <div>
-                <span> { $stops1.length} пересадки </span>
-              </div>
-              <div>
-                { $stops1.join(', ') }
-              </div>
-            </div>
-          </div>
-
-          <div className="ticket__options">
-            <div className="ticket__route">
-              <div>
-                { origin2 } - { destination2 }
-              </div>
-              <div>
-                { date2 }
-              </div>
-            </div>
-            <div className="ticket__duration">
-              <div>
-                <span> в пути </span>
-              </div>
-              <div>
-                { duration2 }
-              </div>
-            </div>
-            <div className="ticket__stops">
-              <div>
-                <span> { $stops2.length} пересадки </span>
-              </div>
-              <div>
-                { $stops2.join(', ') }
-              </div>
-            </div>
-          </div>
+      <div className="ticket__block">
+        <div className="ticket__header">
+          <div className="ticket__price">{ price }</div>
+          <div className="ticket__complogo"><img src={ carrierLogo } /></div>
         </div>
+
+        { Object.values(segments).map( ({ date, destination, duration, origin, stops}, index) => {
+
+            return (
+              <div className="ticket__options">
+                <div className="ticket__route">
+                  <div>
+                    { origin } - { destination }
+                  </div>
+                  <div>
+                    { date }
+                  </div>
+                </div>
+                <div className="ticket__duration">
+                  <div>
+                    <span> в пути </span>
+                  </div>
+                  <div>
+                    { duration }
+                  </div>
+                </div>
+
+                <div className="ticket__stops">
+                  <div>
+                    <span> { Object.values(stops).length } пересадки </span>
+                  </div>
+                  <div>
+                    { Object.values(stops).join(', ') }
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+      </div>
     </li>
   )
 });
-  
+
   return(
     <div className="ticket__container" >
       <ul className="ticket__list" >
@@ -94,4 +64,3 @@ const ticketBlock = props.tickets.map(({ price, carrier, segments: [ { origin, d
 }
 
 export default TicketsList;
-//{ price, carrier, segments: [ { origin, destination, duration, stops: [ first, second, third ] } ] } = props.tickets[i]; // working
